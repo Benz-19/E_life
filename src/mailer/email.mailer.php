0fa8c6a990dcfa0fae9c2b7f17ba49ada2b26066
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-03 17:23:48 +0300

    update

--- src/mailer/email.mailer.php
+++ src/mailer/email.mailer.php
@@ -96,16 +96,16 @@ function sendWithPhpMailer($recipientAddress, $recipientName, $subject, $body)
 function sendEmail($recipientAddress, $recipientName, $subject, $body)
 {
     // Try PHPMailer first
-    if (sendWithPHPMailer($recipientAddress, $recipientName, $subject, $body)) {
-        echo "Email sent using PHPMailer!";
+    // if (sendWithPHPMailer($recipientAddress, $recipientName, $subject, $body)) {
+    //     echo "Email sent using PHPMailer!";
+    // } else {
+    // If PHPMailer fails, fallback to SendGrid
+    if (sendWithMailerSend($recipientAddress, $recipientName, $subject, $body) === true) {
+        echo "Email sent using SendGrid!";
     } else {
-        // If PHPMailer fails, fallback to SendGrid
-        if (sendWithMailerSend($recipientAddress, $recipientName, $subject, $body) === true) {
-            echo "Email sent using SendGrid!";
-        } else {
-            echo "<br>Failed to send email with both SendGrid and PHPMailer.";
-        }
+        echo "<br>Failed to send email with both SendGrid and PHPMailer.";
     }
+    // }
 }
 
 // sendEmail("kingsleyikenna2019@gmail.com", "Kingsley", "Test", "This is to test the email section");
